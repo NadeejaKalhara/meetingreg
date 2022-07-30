4f6739706478654bc16088c0f8ec9c098fa360b6
Nadeeja Kalhara <elkalhara@gmail.com>
2022-07-31 01:10:39 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+const TeleBot = require('telebot');
+var bodyParser = require('body-parser');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const bot = new TeleBot({
+    token: "5403190293:AAEHtiIEaSVESqcIrRmN202eOm5niqZ5_hA", // Required. Telegram Bot API token.
+    polling: { // Optional. Use polling.
+        interval: 1000, // Optional. How often check updates (in ms).
+        timeout: 0, // Optional. Update polling timeout (0 - short polling).
+        limit: 100, // Optional. Limits the number of updates to be retrieved.
+    }});
+    bot.start();
+    bot.sendMessage(-1001615440082,"Hii")
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+var port = 9000;
+
+app.post('/register', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send(req.body);
+    
+    bot.sendMessage(-1001615440082,"Hello")
+});
+
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
